d0eb4a79e7235537f5fdc521860b49d757a770ae
saleh1312 <profsoft1312@gmail.com>
2022-10-26 16:16:01 +0200

    asd

---
+++ draw_3d.py
@@ -0,0 +1,190 @@
+import pygame
+from pygame.locals import *
+import sys
+import cv2
+import numpy as np
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from vision import computer_vision
+from objloder import *
+from PIL import Image
+from PIL import ImageOps
+
+class draw3d:
+    def draw_cube(self):
+        glBegin(GL_QUADS)
+        
+        glColor3f(0.0,1.0,0.0)
+        glVertex3fv((-1.0,1.0,-1.0))
+        glVertex3fv((1.0,1.0,-1.0))
+        glVertex3fv((1.0,-1.0,-1.0))
+        glVertex3fv((-1.0,-1.0,-1.0))
+        
+        glColor3f(1.0,0.0,0.0)
+        glVertex3fv((-1.0,1.0,-1.0))
+        glVertex3fv((1.0,1.0,-1.0))
+        glVertex3fv((1.0,1.0,1.0))
+        glVertex3fv((-1.0,1.0,1.0))
+        
+        glColor3f(0.0,0.0,1.0)
+        glVertex3fv((-1.0,1.0,1.0))
+        glVertex3fv((1.0,1.0,1.0))
+        glVertex3fv((1.0,-1.0,1.0))
+        glVertex3fv((-1.0,-1.0,1.0))
+        
+        
+    
+        glEnd()
+        glColor3f(1.0,1.0,1.0) 
+    def draw_obj(self):
+        
+        glBindTexture(GL_TEXTURE_2D,self.obj)
+    
+        glBegin(GL_TRIANGLES)
+        for i in range(0,len(self.ver),8):
+            v1=(self.ver[i],self.ver[i+1],self.ver[i+2])
+            t1=(self.ver[i+3],self.ver[i+4])
+            glTexCoord2f(t1[0],t1[1]); glVertex3fv(v1)
+        
+        glEnd()
+        glBindTexture(GL_TEXTURE_2D,0)
+    
+    def draw_background(self,frame):
+        im=cv2.flip(frame,0)
+        im=cv2.cvtColor(im,cv2.COLOR_BGR2RGB)
+        im=im.astype(np.float32)
+        glBindTexture(GL_TEXTURE_2D, self.back)
+        
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, im.shape[0], im.shape[1], 0, GL_RGB, GL_UNSIGNED_BYTE, im)
+        
+
+        x=self.x
+        z=self.z
+        glBegin(GL_QUADS)
+        glTexCoord2f(0,1); glVertex3fv((-x, x, -z))
+        glTexCoord2f(1,1); glVertex3fv((x, x, -z))
+        glTexCoord2f(1,0); glVertex3fv((x, -x, -z))
+        glTexCoord2f(0,0); glVertex3fv((-x, -x, -z))
+        glEnd()
+
+        glBindTexture(GL_TEXTURE_2D, 0)
+        
+    def draw(self,frame,mat):
+        
+        self.draw_background(frame)
+        
+        glLoadMatrixd(mat)
+        
+        self.draw_cube()
+        glLoadIdentity()
+         
+        glFlush()
+        
+    def __init__(self,focal_lengh,size):
+
+        self.size=size
+        self.video = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 25, (size,size))
+        
+        self.so=0
+        pygame.init()
+        display = ( size,size)
+        self.screen=pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+        
+        glEnable(GL_TEXTURE_2D);
+        
+        glEnable(GL_DEPTH_TEST)
+        
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        fov=2*(np.arctan(0.5*size/focal_lengh)*180/np.pi)
+        gluPerspective(fov, 1, 0.1, 100.0)
+        
+        
+        self.z=float(100)
+        self.x=(np.tan( (float(fov)/2.0) *(np.pi/180)) )*self.z
+        self.back = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D, self.back)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+        
+        
+        self.obj = glGenTextures(1)
+        obj_image = cv2.imread(r'data\cube.jpg');
+        obj_image=cv2.flip(obj_image,0)
+        obj_image=cv2.cvtColor(obj_image,cv2.COLOR_BGR2RGB)
+        obj_image=obj_image.astype(np.float32)
+    
+    
+        glBindTexture(GL_TEXTURE_2D, self.obj)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, obj_image.shape[0], obj_image.shape[1], 0, GL_RGB, GL_UNSIGNED_BYTE, obj_image)
+    
+        self.ind,self.ver=ObjLoader.load_model(r'data\cube.obj')
+        
+        
+        glMatrixMode(GL_MODELVIEW)
+        
+    
+    def save_video(self):
+        data = glReadPixels(0, 0, self.size, self.size, GL_RGBA, GL_UNSIGNED_BYTE)
+        image = Image.frombytes("RGBA", (self.size, self.size), data)
+        image = ImageOps.flip(image) # in my case image is flipped top-bottom for some reason
+        self.video.write(cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR))
+        
+    def main(self,frame,mat,detect=True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.video.release()
+                pygame.quit()
+                sys.exit()
+                
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        if detect==True:
+            self.draw(frame,mat)
+        else:
+            self.draw_background(frame)
+            
+        self.save_video()
+        pygame.display.flip()
+        
+        pygame.time.wait(10)
+        
+        
+opencvc=computer_vision(r'data\video2.mp4',900,512)
+opengl=draw3d(900,512)
+
+while True:
+    data=opencvc.read()
+    if type(data[0])==type(None):
+        opengl.video.release()
+        break
+    elif type(data[1])==type(None):
+        
+        opengl.main(data[0],None,False)
+    else :
+        
+        opengl.main(data[0],data[1])
+    
+    
+    
+pygame.quit()
+sys.exit()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
---
+++ vision.py
@@ -0,0 +1,111 @@
+import cv2
+import cv2.aruco as aruco
+import numpy as np
+
+
+class computer_vision:
+    def __init__(self,video_name,focal_length,size):
+        self.size=size
+        self.video=cv2.VideoCapture(video_name)
+        self.arucoDict=aruco.Dictionary_get(aruco.DICT_6X6_250)
+        self.arucoparam=aruco.DetectorParameters_create()
+        
+        self.model_points=np.array([
+                                (-1.0, 1.0, -1.0),             
+                                (1.0, 1.0, -1.0),       
+                                (1.0,-1.0,-1.0),   
+                                (-1.0,-1.0,-1.0),     
+                            ])
+    
+        center = (size/2, size/2)
+        self.camera_matrix = np.array(
+                            [[focal_length, 0, center[0]],
+                            [0, focal_length, center[1]],
+                            [0, 0, 1]], dtype = "double"
+                            )
+        self.dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
+        
+    def detect_aruco(self,img):
+        gray =cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        box,ids,rej=aruco.detectMarkers(gray,
+                                        self.arucoDict
+                                        ,parameters=self.arucoparam)
+        
+        return box,ids
+    
+    def convert_to_opengl( self,rvecs,tvecs):
+
+    # build view matrix
+        rmtx = cv2.Rodrigues(rvecs)[0]
+   
+ 
+        view_matrix = np.array([[rmtx[0][0],rmtx[0][1],rmtx[0][2],tvecs[0][0]],
+                            [rmtx[1][0],rmtx[1][1],rmtx[1][2],tvecs[1][0]],
+                            [rmtx[2][0],rmtx[2][1],rmtx[2][2],tvecs[2][0]],
+                            [0.0       ,0.0       ,0.0       ,1.0    ]])
+ 
+        inverse_matrix = np.array([[ 1.0, 1.0, 1.0, 1.0],
+                               [-1.0,-1.0,-1.0,-1.0],
+                               [-1.0,-1.0,-1.0,-1.0],
+                               [ 1.0, 1.0, 1.0, 1.0]])
+ 
+        view_matrix = view_matrix * inverse_matrix
+ 
+        view_matrix = np.transpose(view_matrix)
+ 
+        return view_matrix
+
+    def detect_matrix(self,tl,tr,br,bl):
+        image_points = np.array([
+            tl,tr,br,bl,    
+            ], dtype="double")
+        
+        
+        (success, rotation_vector, translation_vector) = cv2.solvePnP(self.model_points, image_points, self.camera_matrix, self.dist_coeffs)
+
+        return self.convert_to_opengl(rotation_vector, translation_vector)
+    def read(self):
+        if self.video.isOpened():
+            ret, frame = self.video.read()
+        
+            if ret==False:
+                return [None]
+            
+            frame=cv2.resize(frame,(self.size,self.size))
+            box,ids=self.detect_aruco(frame)
+            box=np.array(box)
+            if len(box.shape) !=4:
+                return [frame ,None]
+            
+            tl=(box[0,0,0,0],box[0,0,0,1])
+            tr=(box[0,0,1,0],box[0,0,1,1])
+            br=(box[0,0,2,0],box[0,0,2,1])
+            bl=(box[0,0,3,0],box[0,0,3,1])
+            
+            return [frame ,self.detect_matrix(tl,tr,br,bl),tl,tr,br,bl]
+        else:
+            return [None]
+        
+    def main(self):
+        while(True):
+            data=self.read()
+            if type(data[0])==type(None):
+                break
+            
+            fr=data[0]
+
+            tl,tr,br,bl =data[2],data[3],data[4],data[5],
+            pts = np.array([tl, tr, 
+                br, bl],
+               np.int32)
+    
+            pts = pts.reshape((-1, 1, 2))
+            cv2.polylines(fr,[pts],True,(255,0,0),3)
+            cv2.imshow('frame',fr)
+            if cv2.waitKey(5) & 0xFF == ord('q'):
+                break
+        
+        self.video.release()
+        cv2.destroyAllWindows()
+        
+        
