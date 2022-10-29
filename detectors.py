6b081b41ef796d107cdc0d1ed67527f3309f9db3
saleh1312 <profsoft1312@gmail.com>
2022-10-29 11:27:56 +0200

    sadf

--- detectors.py
+++ detectors.py
@@ -1,6 +1,67 @@
 import cv2
 import cv2.aruco as aruco
 import numpy as np
+import mediapipe as mp
+
+
+
+
+
+class poseDetector:
+    def __init__(self):
+        self.mp_drawing=mp.solutions.drawing_utils
+        self.mp_pose=mp.solutions.pose
+        self.pose=self.mp_pose.Pose(min_detection_confidence=0.5,static_image_mode=True, min_tracking_confidence=0.5)
+    
+    def detect(self,img):
+        shape =img.shape
+        
+        rgbimage = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        rgbimage.flags.writeable = False
+        
+        results = self.pose.process(rgbimage)
+        # Extract landmarks
+        try:
+            landmarks = results.pose_landmarks.landmark
+            lshoulder = [int(landmarks[self.mp_pose.PoseLandmark.LEFT_SHOULDER.value].x*shape[1]),int(landmarks[self.mp_pose.PoseLandmark.LEFT_SHOULDER.value].y*shape[0])]
+            rshoulder = [int(landmarks[self.mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x*shape[1]),int(landmarks[self.mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y*shape[0])]
+            lhip = [int(landmarks[self.mp_pose.PoseLandmark.LEFT_HIP.value].x*shape[1]),int(landmarks[self.mp_pose.PoseLandmark.LEFT_HIP.value].y*shape[0])]
+            rhip = [int(landmarks[self.mp_pose.PoseLandmark.RIGHT_HIP.value].x*shape[1]),int(landmarks[self.mp_pose.PoseLandmark.RIGHT_HIP.value].y*shape[0])]
+            lelbow = [int(landmarks[self.mp_pose.PoseLandmark.LEFT_ELBOW.value].x*shape[1]),int(landmarks[self.mp_pose.PoseLandmark.LEFT_ELBOW.value].y*shape[0])]
+            relbow = [int(landmarks[self.mp_pose.PoseLandmark.RIGHT_ELBOW.value].x*shape[1]),int(landmarks[self.mp_pose.PoseLandmark.RIGHT_ELBOW.value].y*shape[0])]
+            lwrist = [int(landmarks[self.mp_pose.PoseLandmark.LEFT_WRIST.value].x*shape[1]),int(landmarks[self.mp_pose.PoseLandmark.LEFT_WRIST.value].y*shape[0])]
+            rwrist = [int(landmarks[self.mp_pose.PoseLandmark.RIGHT_WRIST.value].x*shape[1]),int(landmarks[self.mp_pose.PoseLandmark.RIGHT_WRIST.value].y*shape[0])]
+            lknee = [int(landmarks[self.mp_pose.PoseLandmark.LEFT_KNEE.value].x*shape[1]),int(landmarks[self.mp_pose.PoseLandmark.LEFT_KNEE.value].y*shape[0])]
+            rknee = [int(landmarks[self.mp_pose.PoseLandmark.RIGHT_KNEE.value].x*shape[1]),int(landmarks[self.mp_pose.PoseLandmark.RIGHT_KNEE.value].y*shape[0])]
+
+            print(lshoulder)
+        except:
+            pass
+
+        # Recolor back to BGR
+        rgbimage.flags.writeable = True
+        rgbimage = cv2.cvtColor(rgbimage, cv2.COLOR_RGB2BGR)
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -44,7 +105,7 @@ class arucoDetector:
         cv2.polylines(fr,[pts],True,(255,0,0),3)
         
     
-    
+
     
 
     
